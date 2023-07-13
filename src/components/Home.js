766ec4bd7fa30bd555fb80b768b11d260daa3a2d
Laura Lima <lauralimaufam@gmail.com>
2023-07-13 02:16:46 -0300

    componente para as paginas + cards

---
+++ src/components/Home.js
@@ -0,0 +1,84 @@
+import Card from 'react-bootstrap/Card';
+
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className='Home-body'>
+            <div className='Home-flexcontainer-item'>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://w1.pngwing.com/pngs/126/188/png-transparent-plastic-bottle-detergent-laundry-detergent-cleaning-housekeeping-dish-detergent-soap-washing-washing-machines.png" />
+                    <Card.Body>
+                        <Card.Title>Detergente</Card.Title>
+                        <Card.Text>
+                            R$ 5.6
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+
+            <div className='Home-flexcontainer-item'>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://w1.pngwing.com/pngs/126/188/png-transparent-plastic-bottle-detergent-laundry-detergent-cleaning-housekeeping-dish-detergent-soap-washing-washing-machines.png" />
+                    <Card.Body>
+                        <Card.Title>Detergente</Card.Title>
+                        <Card.Text>
+                            R$ 5.6
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+
+            <div className='Home-flexcontainer-item'>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://w1.pngwing.com/pngs/126/188/png-transparent-plastic-bottle-detergent-laundry-detergent-cleaning-housekeeping-dish-detergent-soap-washing-washing-machines.png" />
+                    <Card.Body>
+                        <Card.Title>Detergente</Card.Title>
+                        <Card.Text>
+                            R$ 5.6
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+
+            <div className='Home-flexcontainer-item'>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://w1.pngwing.com/pngs/126/188/png-transparent-plastic-bottle-detergent-laundry-detergent-cleaning-housekeeping-dish-detergent-soap-washing-washing-machines.png" />
+                    <Card.Body>
+                        <Card.Title>Detergente</Card.Title>
+                        <Card.Text>
+                            R$ 5.6
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+
+            <div className='Home-flexcontainer-item'>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://w1.pngwing.com/pngs/126/188/png-transparent-plastic-bottle-detergent-laundry-detergent-cleaning-housekeeping-dish-detergent-soap-washing-washing-machines.png" />
+                    <Card.Body>
+                        <Card.Title>Detergente</Card.Title>
+                        <Card.Text>
+                            R$ 5.6
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+
+            <div className='Home-flexcontainer-item'>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://w1.pngwing.com/pngs/126/188/png-transparent-plastic-bottle-detergent-laundry-detergent-cleaning-housekeeping-dish-detergent-soap-washing-washing-machines.png" />
+                    <Card.Body>
+                        <Card.Title>Detergente</Card.Title>
+                        <Card.Text>
+                            R$ 5.6
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+        </div>
+
+    );
+}
+
+export default Home;
